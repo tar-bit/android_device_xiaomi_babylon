5608ad6a262eb92d09b4de7fd0c07a4aa046cab0
tar-bit <tecnocracy@proton.me>
2025-03-09 15:59:04 -0400

    Update lineage_babylon.mk

--- lineage_babylon.mk
+++ lineage_babylon.mk
@@ -21,16 +21,16 @@ $(call inherit-product, device/xiaomi/babylon/device.mk)
 PRODUCT_DEVICE := babylon
 PRODUCT_NAME := lineage_babylon
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := 2211133G
+PRODUCT_MODEL := 2308CPXD0C
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_NAME := 2211133G
-PRODUCT_SYSTEM_DEVICE := 2211133G
+PRODUCT_SYSTEM_NAME := 2308CPXD0C
+PRODUCT_SYSTEM_DEVICE := 2308CPXD0C
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc=$(call normalize-path-list, "babylon_global-user 15 TKQ1.221114.001 V816.0.5.0.VMVCNXM release-keys")
+    BuildDesc=$(call normalize-path-list, "babylon_global-user 15 TKQ1.221114.001 OS2.0.101.0.VMVCNXM release-keys")
 
-BUILD_FINGERPRINT := Xiaomi/babylon_global/babylon:15/TKQ1.221114.001/V816.0.5.0.VMVCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/babylon_global/babylon:15/TKQ1.221114.001/OS2.0.101.0.VMVCNXM:user/release-keys
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
